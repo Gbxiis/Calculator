58b0db63d85959cb279c2383b080eafc13541fb3
Gabriel Bomfim <gabrielbomfimfelipe@gmail.com>
2023-02-05 16:23:31 -0300

    ideia concluida

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
-const numbers = document.querySelectorAll(".numbers");
+const numbers = document.querySelectorAll(".characters");
 const result = document.getElementById("resulted");
 
-var firstClick;
-var lastClick;
+let input = "";
+let output;
+let firstput;
+let lastput;
+let operator = "";
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", e => {
-    let atr = e.target.getAttribute("value");
+const overview = (res) => {
+  result.textContent = res;
+};
+const calculater = () => {
+  switch (operator) {
+    case "+":
+      return firstput + lastput;
+      break;
+    case "-":
+      return firstput - lastput;
+    case "x":
+      return firstput * lastput;
+      break;
+    case "÷":
+      return firstput / lastput;
+      break;
+    case "%":
+      return (firstput / 100) * lastput;
+      break;
+    default:
+      break;
+  }
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    const value = e.currentTarget.textContent;
+    const id = e.currentTarget.id;
+    console.log(id);
+
+    if (input === "" && value === "0") return;
+
+    if (id !== "operation" && input !== "0") {
+      input += value;
+      overview(input);
+    }
+    if (id === "operation") {
+
+      
+      if (value === "=" && firstput && lastput) {
+        result = calculater();
+        firstput = result;
+        input = result;
+        lastput = null;
+        overview(result);
+      }
+    }
   });
-}
+});
